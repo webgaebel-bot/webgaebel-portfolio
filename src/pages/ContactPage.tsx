94312d6b1df82752b921b6d1eb61b355668ad4fb
HakamTechSol <alhakamsofts@gmail.com>
2026-07-15 14:26:33 +0500

    remove-all-things

--- src/pages/ContactPage.tsx
+++
@@ -1,9 +0,0 @@
-import Contact from '../components/Contact';
-
-export default function ContactPage() {
-  return (
-    <main className="min-h-screen pt-20">
-      <Contact />
-    </main>
-  );
-}
